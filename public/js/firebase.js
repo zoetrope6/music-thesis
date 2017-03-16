e6cfb23f032339895c55c5d0e59a04441a3d8935
Zoe Rogers <zrogers2@cca.edu>
2017-03-16 16:09:40 -0700

    merged_barebone_?

---
+++ public/js/firebase.js
@@ -0,0 +1,8 @@
+var config = {
+        apiKey: "AIzaSyBPJoVpj7rdKJ-TgMgLnFyc4FZSAnJLx50",
+        authDomain: "key",
+        databaseURL: "https://zoe-project-ca882.firebaseio.com",
+        storageBucket: "<BUCKET>.appspot.com",
+        messagingSenderId: "<SENDER_ID>",
+      };
+      firebase.initializeApp(config);
